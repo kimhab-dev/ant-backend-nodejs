9042fb27a3bf449cf0cc25c514dad69973b707b9
kimhab <kimhabsok9@gmail.com>
2026-03-02 16:50:38 +0700

    global obj: interval

--- 1.mouykea/app.js
+++ 1.mouykea/app.js
@@ -3,3 +3,14 @@ let name = 'ANT';
 
 console.log(i);
 console.log(name);
+
+let count = 0;
+let intervalID = setInterval(() => {
+    console.log('Set interval');
+
+    count++;
+
+    if (count === 5) {
+        clearInterval(intervalID);
+    };
+}, 2000)

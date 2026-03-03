4fd884799f2e5856a8952a10b0d59b6b4b471892
kimhab <kimhabsok9@gmail.com>
2026-03-03 16:48:02 +0700

    node

--- 2.basic-module/exercise/app.js
+++ 2.basic-module/exercise/app.js
@@ -2,6 +2,14 @@ const sum = require('./sum.js');
 const minus = require('./minus.js');
 const mul = require('./mul.js');
 
+const calculat = require('./calculat.js')
+
 console.log("Sum", sum(10, 20));
 console.log("Minus", minus(20, 10));
 console.log("Multiple", mul(20, 10));
+
+console.log("All calculate",
+    calculat.sum(10, 20),
+    calculat.minus(10, 20),
+    calculat.mul(10, 20),
+)
---
+++ 2.basic-module/exercise/calculat.js
@@ -0,0 +1,17 @@
+const sum = require("./sum");
+
+function minus(num1, num2) {
+    return num1 - num2;
+};
+
+function mul(num1, num2) {
+    return num1 * num2;
+};
+
+function minus(num1, num2) {
+    return num1 - num2;
+};
+
+module.exports = {
+    minus, mul, sum
+}

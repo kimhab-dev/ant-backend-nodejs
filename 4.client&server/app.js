9d53ab65d4379cf4d385ad9bbdf1aa05af09e932
kimhab <kimhabsok9@gmail.com>
2026-03-05 16:09:36 +0700

    server&client: manage route in node

--- 4.client&server/app.js
+++ 4.client&server/app.js
@@ -2,13 +2,28 @@ const http = require('http');
 const fs = require('fs');
 
 const server = http.createServer((req, res) => {
-    console.log(req.url, req.method);
+    // console.log(req.url, req.method);
 
     // respone tv kan user vinh jea string. but lus tra res end ban res tv nam
     res.setHeader('content-Type', 'text/html');
     // res.setHeader('content-Type', 'text/plain');
     // res.setHeader('Content-Type', 'application/json');
-    fs.readFile('4.client&server/index.html', (err, data) => {
+
+    let url = req.url;
+    let path = '';
+    switch (url) {
+        case '/':
+            path = '4.client&server/index.html';
+            res.statusCode = 200;
+            break;
+        case '/contact':
+            path = '4.client&server/contact.html';
+            break;
+        default:
+            path = '4.client&server/404.html'
+            break;
+    }
+    fs.readFile(path, (err, data) => {
         if (err) console.log(err);
         // res.write(data);
         res.end(data);

a4670538bfa39b4daa5a830c297fb7c46b5f91a7
kimhab <kimhabsok9@gmail.com>
2026-03-06 14:31:28 +0700

    node npm

--- 4.client&server/app.js
+++ 4.client&server/app.js
@@ -1,33 +1,36 @@
 const http = require('http');
-const fs = require('fs');
+// const fs = require('fs');
 
 const server = http.createServer((req, res) => {
     // console.log(req.url, req.method);
-
     // respone tv kan user vinh jea string. but lus tra res end ban res tv nam
-    res.setHeader('content-Type', 'text/html');
+    // res.setHeader('content-Type', 'text/html');
     // res.setHeader('content-Type', 'text/plain');
-    // res.setHeader('Content-Type', 'application/json');
+    res.setHeader('Content-Type', 'application/json');
 
-    let url = req.url;
-    let path = '';
+    let url = req.url.toLowerCase();
+    // let path = '';
     switch (url) {
         case '/':
-            path = '4.client&server/index.html';
-            res.statusCode = 200;
+            // path = '4.client&server/index.html';
+            // res.statusCode = 200;
+            res.write('{flag: "home"}');
             break;
-        case '/contact':
-            path = '4.client&server/contact.html';
+        case '/labs':
+            // path = '4.client&server/contact.html';
+            res.write('{flag: "labs"}');
             break;
         default:
-            path = '4.client&server/404.html'
+            // path = '4.client&server/404.html'
+            res.write("404");
             break;
     }
-    fs.readFile(path, (err, data) => {
-        if (err) console.log(err);
-        // res.write(data);
-        res.end(data);
-    })
+    res.end();
+    // fs.readFile(path, (err, data) => {
+    //     if (err) console.log(err);
+    //     // res.write(data);
+    //     res.end(data);
+    // })
 
 });
 
